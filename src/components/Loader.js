4f1e728ca48a00527a8c41a07b0af04f57575c22
tsayush2390 <115918401+tsayush2390@users.noreply.github.com>
2025-04-17 00:22:30 +0530

    Update Loader.js

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import {Stack } from '@mui/material'
-import { InfinitySpin } from 'react-loader-spinner'
+import { Stack } from '@mui/material'
+
+const loaderStyle = {
+  border: '6px solid #e0e0e0',
+  borderTop: '6px solid #3f51b5',
+  borderRadius: '50%',
+  width: '40px',
+  height: '40px',
+  animation: 'spin 1s linear infinite'
+}
+
+const keyframesStyle = `
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+`
 
 const Loader = () => {
   return (
-    <Stack direction='row' justifyContent='center' alignItems='center' width='100%'>
-      <InfinitySpin color='gray' />
-    </Stack>
+    <>
+      <style>{keyframesStyle}</style>
+      <Stack direction='row' justifyContent='center' alignItems='center' width='100%' height='100%'>
+        <div style={loaderStyle} />
+      </Stack>
+    </>
   )
 }
 
